1ceff3cf871ea5deb911eea632880a99aa5b8afc
adesh dandawate <avdandawate1996@gmail.com>
2018-06-21 22:39:33 +0530

    Database Connectivity

---
+++ api/db/db.php
@@ -0,0 +1,4 @@
+<?php
+$con = mysqli_connect("localhost","root","","hbs") or die(mysql_error());
+
+?>
--- index.php
+++ index.php
@@ -1,11 +1,22 @@
 <html>
 <?php require('common/header.php') ?>
+<?php require('api/db/db.php') ?>
 <body>
-    Hey there!!
-	Hiii!!!
-	change
-What the heck man!
-ff git l
+<?php
+
+$result = mysqli_query($con,'select * from roles');
+
+while($row = mysqli_fetch_assoc($result))
+	echo $row['id']."\t".$row['name']."<br>"
+
+
+?>
+
+<form action="/api/menu_entry.php">
+<input type="text" value="" name="roleName">
+<button type="submit">Insert</button>
+</form>
+
 </body>
 <?php require('common/footer.php') 
 
