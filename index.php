48c669d4ac2e50d44539bcfa8ccd5a0cbd6b9b13
adesh dandawate <avdandawate1996@gmail.com>
2018-06-22 15:15:15 +0530

    Adesh changed index

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php require('common/header.php'); ?>
 <body>
 <?php require('api/db/db.php'); ?>
-<button class="btn btn-primary">YOLO</button>
+<!--<button class="btn btn-primary">YOLO</button>-->
 <table class="table table-bordered table-hover" id="roleTable">
 	<thead>
 	<tr>
